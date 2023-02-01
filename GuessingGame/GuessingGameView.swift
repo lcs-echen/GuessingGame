7ea1fe612e597259abf2b621dedcc1fc63dc3b19
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-01 11:01:45 -0500

    Changed the slider to the text-based input

--- GuessingGame/GuessingGameView.swift
+++ GuessingGame/GuessingGameView.swift
@@ -14,43 +14,56 @@ struct GuessingGameView: View {
     
     // The input collected from the Slider
     // ADD MISSING CODE HERE
-    @State var data: Double = 50
+    @State var guessAsString: String = ""
     // The secret target that the user is trying to guess
     @State var target = Int.random(in: 0...100)
     
     // What feedback to provide the user with
     @State var feedback = ""
     
+    
     // MARK: Computed properties
+    var guessAsInteger: Int? {
+        guard let unwarppedValue = Int(guessAsString) else {
+            return nil
+        }
+        return unwarppedValue
+    }
     var body: some View {
         
         VStack {
             // Collect the user's input using a Slider
             // ADD MISSING CODE HERE
-                Slider(value: $data,
-                       in: 0...100,
-                       step: 1.0,
-                       label: { Text("data") },
-                       minimumValueLabel: { Text("0") },
-                       maximumValueLabel: { Text("100") } )
+            
+            TextField("Enter your guess", text: $guessAsString)
+            
+//                Slider(value: $data,
+//                       in: 0...100,
+//                       step: 1.0,
+//                       label: { Text("data") },
+//                       minimumValueLabel: { Text("0") },
+//                       maximumValueLabel: { Text("100") } )
         
             // Show the user's input to them using a Text view
             // ADD MISSING CODE HERE
-            Text("\(data.formatted(.number.precision(.fractionLength(0))))")
+//            Text("\(data.formatted(.number.precision(.fractionLength(0))))")
             // Let the user check to see if their current guess is correct
             Button(action: {
                 
                 // Convert the user's input, which is a Double, into an Int
-                let currentGuessAsInteger = Int(data)
+                guard let guess = guessAsInteger else {
+                    feedback = "Please provide a integer between 0 and 100."
+                    return
+                }
                 
                 // Compare the user's guess to the target and give appropriate feedback
                 // ADD MISSING CODE HERE
-                if currentGuessAsInteger == target{
-                    return feedback = "correct"
-                }else if currentGuessAsInteger < target{
-                    return feedback = "too small"
+                if guess == target {
+                    feedback = "correct"
+                }else if guess < target{
+                    feedback = "too small"
                 }else{
-                    return feedback = "too large"
+                    feedback = "too large"
                 }
                     
             }, label: {
