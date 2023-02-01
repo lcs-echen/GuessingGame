03717bbbbaa04d821b6947012b4c1b4944696292
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-01 10:16:24 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Evelyn Chen on 2023-02-01.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
